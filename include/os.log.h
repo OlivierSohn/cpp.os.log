5b28767c996ee2b6db21704dced57d250cdd1c52
Olivier Sohn <olivier.sohn@gmail.com>
2015-02-16 06:48:42 +0100

    assert replaced by custom Macro

--- include/os.log.h
+++ include/os.log.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#if defined ( WIN32 )
+#define __func__ __FUNCTION__
+#endif
+
 #include <stdarg.h>
 
 typedef enum logLevel
@@ -10,3 +14,54 @@ typedef enum logLevel
 }logLevel;
 
 void LG(logLevel, /*const char* sModule,*/ const char * format, ...);
+
+#define ERR_LOG(x,type) do{ LG(ERR, "%s (%s) failed\n   |in %s\n   |(%s,%d)", #type, #x, __func__, __FILE__, __LINE__ ); } while(0)
+
+// "soft" Assert
+#define C(x) if(!(x)) {ERR_LOG(x,Check);} else do{}while(0)
+
+/* 
+"A" is used to replace
+
+// assert(x);
+
+with
+
+// A(x);
+
+with the advantage that :
+- a comprehensive error message is logged, and the stack is available in debugger (not always the case with assert)
+- in release, code execution is not broken buit an error message is logged
+*/
+
+#ifndef NDEBUG
+#define ASSERT_THROW do{throw;}while(0)
+#else
+#define ASSERT_THROW do{}while(0)
+#endif
+
+#define ASSERT_ERR_LOG(x) ERR_LOG(x,Assertion)
+#define ASSERT_ERR(x) do{ASSERT_ERR_LOG(x); ASSERT_THROW;}while(0)
+#define if_A(x) if(!(x)) ASSERT_ERR(x); else
+#define A(x) if_A(x) do{}while(0)
+
+/* 
+"if_A" is used to replace 
+
+//  if(x)
+//  {
+//      // user code
+//  }
+//  else
+//  {
+//      assert(x);
+//      LG(ERR,"x is NULL");
+//  }
+
+with
+
+//  if_A(x)
+//  {
+//      // user code
+//  }
+*/
