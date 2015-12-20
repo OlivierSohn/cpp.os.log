1fb2be427c80f38b1829f1bd8f3e7a13bd915baa
Olivier Sohn <olivier.sohn@gmail.com>
2015-12-20 03:32:28 -0500

    fixed : name of solver variable should not contain '-' or '{,}'

--- include/os.log.format.h
+++ include/os.log.format.h
@@ -3,8 +3,12 @@
 #include <ctime>
 #include <string>
 #include <string>
+#include <iostream>
 #include <sstream>
 #include <vector>
+#include <functional>
+#include <algorithm>
+#include <cctype>
 
 namespace imajuscule
 {
@@ -13,4 +17,26 @@ namespace imajuscule
     
     void FormatDate(tm*time, std::string&oDate);
     bool iequals(const std::string& a, const std::string& b);
+    
+    
+    template<char ... CharacterList>
+    inline bool check_characters(char c) {
+        char match_characters[sizeof...(CharacterList)] = { CharacterList... };
+        for(int i = 0; i < sizeof...(CharacterList); ++i) {
+            if(c == match_characters[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    template<char ... CharacterList>
+    inline void strip_characters(std::string & str) {
+        str.erase(std::remove_if(str.begin(), str.end(), &check_characters<CharacterList...>), str.end());
+    }
+    
+    inline std::string alphaNum(std::string s) {
+        s.erase(std::remove_if(s.begin(), s.end(), std::not1(std::function<int(int)>((int(*)(int))std::isalnum))), s.end());
+        return s;
+    }
 }
