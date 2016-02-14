df484a2f0b72bd82c24b6b22ce8ceee1bf9fc738
Olivier Sohn <olivier.sohn@gmail.com>
2016-02-14 15:39:48 -0500

    logs

--- include/os.log.h
+++ include/os.log.h
@@ -19,12 +19,12 @@ typedef enum logLevel
 void LG(logLevel, /*const char* sModule,*/ const char * format, ...);
 
 #define ERR_LOG(x,type) do{ \
-LG(ERR, "%s (%s) failed\n   |in %s\n   |(%s,%d)", #type, #x, __func__, __FILE__, __LINE__ ); \
+LG(ERR, "%s : ' %s '\n   |in %s\n   |(%s,%d)", #type, #x, __func__, __FILE__, __LINE__ ); \
 logStack(); \
 } while(0)
 
 // "soft" Assert
-#define C(x) if(!(x)) {ERR_LOG(x,Check);} else do{}while(0)
+#define C(x) if(!(x)) {ERR_LOG(x,check);} else do{}while(0)
 
 /* 
 "A" is used to replace "assert(x)" with "A(x)"
@@ -40,7 +40,7 @@ with the advantage that :
 #define ASSERT__THROW do{}while(0)
 #endif
 
-#define ASSERT_ERR_LOG(x) ERR_LOG(x,Assertion)
+#define ASSERT_ERR_LOG(x) ERR_LOG(x,assert)
 #define ASSERT_ERR(x) do{ASSERT_ERR_LOG(x); ASSERT__THROW;}while(0)
 
 #ifdef _MSC_VER
