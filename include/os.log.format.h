186f0e4ff9fbf82599a1118e2c3866d88b0eb3a7
Olivier Sohn <olivier.sohn@gmail.com>
2016-04-26 00:08:17 -0400

    minimalistic script engine

--- include/os.log.format.h
+++ include/os.log.format.h
@@ -2,18 +2,18 @@
 
 #include <ctime>
 #include <string>
-#include <string>
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <functional>
 #include <algorithm>
 #include <cctype>
+#include <locale>
 
 namespace imajuscule
 {
     void split_in_lines(const std::string &s, char delim, std::vector<std::string> &elems);
-    std::vector<std::string> split_in_lines(const std::string &s, char delim);
+    std::vector<std::string> split_in_lines(const std::string &s, char delim = '\n');
     std::vector<std::string> Tokenize(const std::string& str, const std::string& delimiters = " ");
     
     void FormatDate(tm*time, std::string&oDate);
@@ -47,5 +47,44 @@ namespace imajuscule
     }
     inline std::string opposite( std::string && s ) {
         return "-(" + s + ")";
+    }
+    
+    inline int begins_with(std::string const& s, std::string && begin) {
+        return s.compare(0, begin.size(), begin) ? 0: (int) begin.size();
+    }
+    
+    
+    // trim from start (in place)
+    inline void ltrim(std::string &s) {
+        s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+    }
+    
+    // trim from end (in place)
+    inline void rtrim(std::string &s) {
+        s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    }
+    
+    // trim from both ends (in place)
+    inline void trim(std::string &s) {
+        ltrim(s);
+        rtrim(s);
+    }
+    
+    // trim from start (copying)
+    inline std::string ltrimmed(std::string s) {
+        ltrim(s);
+        return s;
+    }
+    
+    // trim from end (copying)
+    inline std::string rtrimmed(std::string s) {
+        rtrim(s);
+        return s;
+    }
+    
+    // trim from both ends (copying)
+    inline std::string trimmed(std::string s) {
+        trim(s);
+        return s;
     }
 }
