c881bbff8a1ee4073ee2e4485e9a9ad5b8de6a69
Olivier Sohn <olivier.sohn@gmail.com>
2017-02-18 20:57:11 +0100

    constexpr modifications, ramps are now better controlled in time

--- include/os.log.content.h
+++ include/os.log.content.h
@@ -2,4 +2,14 @@
 
 namespace imajuscule {
     void logWithLineNumbers(std::string const &);
+
+    template<typename T>
+    void logRange(range<T> r) {
+        if(r.empty()) {
+            LG(INFO, "empty");
+        }
+        else {
+            LG(INFO, "%f .. %f", r.getMin(), r.getMax());
+        }
+    }
 }
