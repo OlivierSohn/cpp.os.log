e4e8da1a4376d57c18b0985576d052a22a51aa2f
Olivier Sohn <olivier.sohn@gmail.com>
2017-03-21 17:33:20 +0100

    grey noise computation, minor changes

--- include/os.log.h
+++ include/os.log.h
@@ -92,4 +92,13 @@ with the advantage that :
     private:
         T & ref;
     };
+    
+    struct ScopedLog {
+        ScopedLog(const char * action, const char * str) : action(action) {
+            std::cout << "--> " << action << " " << str << " ... " << std::endl;
+        }
+        ~ScopedLog() {std::cout << "--> " << action << " Done" << std::endl;}
+    private:
+        const char * action;
+    };
 }
