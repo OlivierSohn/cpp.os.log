14c2eaa84cd64df687aa084d9cfa7b2d644632ab
Olivier Sohn <olivier.sohn@gmail.com>
2018-04-23 20:05:13 +0200

    Change paths

--- include/public.h
+++ include/public.h
@@ -12,7 +12,7 @@
 #include <string>
 #include <vector>
 
-#include "../../algorithms/include/public.h"
+#include "../../cpp.algorithms/include/public.h"
 
 #include "os.log.h"
 #include "os.log.content.h"
