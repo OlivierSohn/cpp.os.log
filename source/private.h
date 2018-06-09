2c26aa9611883873996770545458e2521c8002f7
Olivier Sohn <olivier.sohn@gmail.com>
2018-06-09 20:09:34 +0200

    Windows

--- source/private.h
+++ source/private.h
@@ -3,19 +3,3 @@
 #ifdef __ANDROID__
 #include <android/log.h>
 #endif
-
-#ifdef _WIN32
-#include <process.h>
-#  ifndef NOMINMAX
-#    define NOMINMAX
-#  endif
-#include <Windows.h>
-#include "dbghelp.h"
-#pragma comment(lib, "Dbghelp.lib")
-
-#include <cstdlib>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#endif
